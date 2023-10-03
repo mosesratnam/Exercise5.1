133bb8e2054985d584f989f29f7e850d4ed70aea
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-03 13:24:39 +0100

    A c# program for exercise 5.1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise5._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #nullable disable
+
+        string sentence = "Current sentence: ";
+        string userWord;
+        bool userInput = true;
+
+        Console.WriteLine("Welcome to Exercise 5.1 - Pre Learning!");
+        Console.WriteLine("=======================================");
+        Console.WriteLine();
+
+        while (userInput)
+        {
+            Console.Write("Enter a word: ");
+            userWord = Console.ReadLine();
+            if (userWord != ""){
+                sentence += String.Format(" {0}", userWord);
+                Console.WriteLine(sentence);
+            } else {
+                userInput = false;
+            }
+        }
+    }
+}
